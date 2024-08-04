9d214a4824311c2f90f4085f838e3744f08767d3
kumuditha-udayanga <kumudithaudaya@gmail.com>
2024-08-04 11:34:47 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const config = {
+    PORT: process.env.port,
+    SUBMISSION_SERVICE_URL: process.env.submission_service_url,
+    DELIVERY_SERVICE_URL: process.env.deliver_service_url
+};
